4b6be8142d209648c1a85ac2ce89b1d8cd94869f
Brian Huynh <vuduchv@gmail.com>
2022-04-30 20:12:09 +1000

    Portfolio in progress

--- main.js
+++ main.js
@@ -34,4 +34,19 @@ $(document).ready(function () {
         
         $('html, body').scrollTop(0);
     });
+
 });
+
+
+
+
+// $(".skillbar").each(function () {
+//     $(this)
+//       .find(".skillbar-bar")
+//       .animate(
+//         {
+//           width: $(this).attr("data-percent")
+//         },
+//         2500
+//       );
+//   });
