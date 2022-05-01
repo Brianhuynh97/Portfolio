d4db6c5a18578f1a0c6e3fa5bee42a345cd35cdb
Brian Huynh <vuduchv@gmail.com>
2022-05-01 21:04:00 +1000

    Finished Design

--- main.js
+++ main.js
@@ -34,6 +34,18 @@ $(document).ready(function () {
 
     $('html, body').scrollTop(0);
   });
+
+
+  $(window).scroll(function () {
+    $('.fadedfx').each(function () {
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow + 500) {
+        $(this).addClass("fadeIn");
+      }
+    });
+  });
 });
 
 
